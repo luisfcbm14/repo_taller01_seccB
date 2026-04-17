c51132f165968c2656c6b32b92f021ec056846e6
luisfcbm14 <luisalvarezg2007@gmail.com>
2026-04-17 14:52:53 -0400

    agregamos el directorio

--- Persistencia01B/Persistencia01B/Program.cs
+++ Persistencia01B/Persistencia01B/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: usuario
+ * Usuario: Luis Alvarez
  * Fecha: 17/4/2026
  * Hora: 2:16 p. m.
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace Persistencia01B
 {
@@ -16,8 +17,22 @@ namespace Persistencia01B
 		{
 			Console.WriteLine("Taller Sección B");
 			
-			// TODO: Implement Functionality Here
+			// directorio
 			
+			string rutaRaiz  = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DATOSIUJO");
+			
+			string rutaReportes = Path.Combine(rutaRaiz, "REPORTES");
+			Console.WriteLine(rutaRaiz);
+			Console.WriteLine(rutaReportes);
+			
+			if(!Directory.Exists(rutaReportes)){
+			//crear el directorio "reportes"
+				Directory.CreateDirectory(rutaReportes);
+				Console.WriteLine("Directorio creado correctamente");
+			   }
+			
+			//
+				
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
